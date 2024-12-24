33537c892c5adec94807cae34ef2ae34d7944e51
Chun-Chieh Li <ccli8@nuvoton.com>
2024-12-24 11:13:25 +0800

    Migrate to Mbed CE
    1.  Common change for migration to Mbed CE from Mbed CLI 1/2
        (1) Remove *.lib originally for library import. Replace with git submodule.
        (2) Rename mbed_app.json/mbed_lib.json to mbed_app.json5/mbed_lib.json5
        (3) Add/update CMakeLists.txt
        (4) Remove .mbedignore originally for Mbed CLI 1
        (5) Update document
    2.  Support VS Code development
    3.  Refine main.cpp
        (1) Support full response receive, not just partial
        (2) Refactor

--- README.md
+++ README.md
@@ -1,57 +1,151 @@
-# Getting started with WiFi on mbed OS
+# Example for multiple TCP connections on Nuvoton's Mbed CE enabled boards
 
+This is an example to demo multiple TCP connections over WiFi and Ethernet
+on Nuvoton's Mbed CE enabled boards.
 
-### Import NuMaker-mbed-wifi-tcp from on-line IDE
-1. Please choose Nuvoton NuMaker-PFM-XXX as your target platform.
-2. Please press the left-up icon "New", then choose "WiFi TCP Example" from the template list.
-3. After new your WiFi program, please also import ESP8266 lib by pressing the left-up icon "Import".
-4. Import from URL "https://github.com/ARMmbed/esp8266-driver/"
-5. If the last version of ESP8266 can't work well, please switch to ver. 6:450cc12 .
+Check out [Mbed CE](https://github.com/mbed-ce)
+for details on Mbed OS community edition.
 
-### Adjust Your WiFi Setting in mbed_app.json
-1. wifi-ssid: fill your WiFi AP SSID
-2. wifi-password: fill the WiFi password
+## Support development tools
 
-#### Now compile
-Please press compile icon.
+Use cmake-based build system.
+Check out [hello world example](https://github.com/mbed-ce/mbed-ce-hello-world) for getting started.
 
-#### Burn Code & Execute
-1. Connect NuMaker-PFM-XXX with your PC by USB cable, then there will be one "mbed" disk.
-2. Drag & drop the built binary into "mbed" disk on you PC.
-3. Press device's reset button and you could get the below log by uart console.
--------------------------------------------------------------------------------
- Start WiFi test
- Start Connection ...
+> **⚠️ Warning**
+>
+> Legacy development tools below are not supported anymore.
+> - [Arm's Mbed Studio](https://os.mbed.com/docs/mbed-os/v6.15/build-tools/mbed-studio.html)
+> - [Arm's Mbed CLI 2](https://os.mbed.com/docs/mbed-os/v6.15/build-tools/mbed-cli-2.html)
+> - [Arm's Mbed CLI 1](https://os.mbed.com/docs/mbed-os/v6.15/tools/developing-mbed-cli.html)
 
-Using WiFi
+For [VS Code development](https://github.com/mbed-ce/mbed-os/wiki/Project-Setup:-VS-Code)
+or [OpenOCD as upload method](https://github.com/mbed-ce/mbed-os/wiki/Upload-Methods#openocd),
+install below additionally:
+
+-   [NuEclipse](https://github.com/OpenNuvoton/Nuvoton_Tools#numicro-software-development-tools): Nuvoton's fork of Eclipse
+-   Nuvoton forked OpenOCD: Shipped with NuEclipse installation package above.
+    Checking openocd version `openocd --version`, it should fix to `0.10.022`.
+
+## Developer guide
+
+In the following, we take **NuMaker-IoT-M467** board as an example for Mbed CE support.
+
+### Hardware requirements
+
+-   NuMaker-IoT-M467 board
+-   Host OS: Windows or others
+-   Ethernet cable (RJ45)
+-   WiFi ESP8266
+
+### Build the example
+
+1.  Clone the example and navigate into it
+    ```
+    $ git clone https://github.com/mbed-nuvoton/NuMaker-mbed-ce-multi-connect-tcp-example
+    $ cd NuMaker-mbed-ce-multi-connect-tcp-example
+    $ git checkout -f master
+    ```
+
+1.  Deploy necessary libraries
+    ```
+    $ git submodule update --init
+    ```
+    Or for fast install:
+    ```
+    $ git submodule update --init --filter=blob:none
+    ```
+
+1.  Compile with cmake/ninja
+    ```
+    $ mkdir build; cd build
+    $ cmake .. -GNinja -DCMAKE_BUILD_TYPE=Develop -DMBED_TARGET=NUMAKER_IOT_M467
+    $ ninja
+    $ cd ..
+    ```
+
+### Flash the image
+
+Flash by drag-n-drop built image `NuMaker-mbed-ce-multi-connect-tcp-example.bin` or `NuMaker-mbed-ce-multi-connect-tcp-example.hex` onto **NuMaker-IoT-M467** board
+
+### Verify the result
+
+On host terminal (115200/8-N-1), you should see:
+
+```
+Start Connection ...
 
 Connecting to WiFi..
 
-Connected to Network successfully
-TCP client IP Address is 192.168.43.68
- HTTP Connection ...
-HTTP: Connected to developer.mbed.org:80
-HTTP: Received 461 chars from server
-HTTP: Received 200 OK status ... [OK]
-HTTP: Received 'Hello world!' status ... [OK]
-HTTP: Received massage:
+Connected to Network (WiFi) successfully
+WiFi TCP client IP Address is 192.168.1.107
 
+Connecting to ETH..
+mac address f6-bf-01-0b-f0-06
+PHY ID 1:0x1c
+PHY ID 2:0xc816
+
+Connected to Network (ETH) successfully
+Eth TCP client IP Address is 192.168.0.100
+
+WiFi HTTP Connection ...
+WiFi HTTP: Connected to www.ifconfig.io:80
+ETH HTTP Connection ...
+ETH HTTP: Connected to www.ifconfig.io:80
+
+
+Current heap: 4341
+Max heap size: 4965
+
+Test round 1 ...
+
+Test WiFi TCP client
+[WiFi]HTTP: Request message:
+GET /method HTTP/1.1
+Host: www.ifconfig.io
+
+
+
+[WiFi]HTTP: Received 786 chars from server
+[WiFi]HTTP: Received 200 OK status ... [OK]
+[WiFi]HTTP: Received message:
 HTTP/1.1 200 OK
-Server: nginx/1.7.10
-Date: Thu, 29 Sep 2016 05:55:06 GMT
-Content-Type: text/plain
-Content-Length: 14
-Connection: close
-Last-Modified: Fri, 27 Jul 2012 13:30:34 GMT
-Accept-Ranges: bytes
-Cache-Control: max-age=36000
-Expires: Thu, 29 Sep 2016 15:55:06 GMT
-X-Upstream-L3: 172.17.0.4:80
-X-Upstream-L2: developer-sjc-cyan-1-nginx
-X-Upstream-L1-next-hop: 217.140.101.22:8001
-X-Upstream-L1: developer-sjc-cyan-border-nginx
+Date: Thu, 26 Dec 2024 07:44:29 GMT
+Content-Type: text/plain; charset=utf-8
+Content-Length: 4
+Connection: keep-alive
+cf-cache-status: DYNAMIC
+Report-To: {"endpoints":[{"url":"https:\/\/a.nel.cloudflare.com\/report\/v4?s=aDuZlS6MnzZue0%2BPg7J%2FpfCjeMba7h37Ux7v70LOlh%2B5t%2Bvzo%2BvHjq8TxG5lkmWj8QEXgt%2Bl5we1zsTvsdYrRsOs9nKqqWomnfwnuEru9eaBDITH5iwlMJP7d1woTmwGWIg%3D"}],"group":"cf-nel","max_age":604800}
+NEL: {"success_fraction":0,"report_to":"cf-nel","max_age":604800}
+Server: cloudflare
+CF-RAY: 8f7f72251f618451-TPE
+alt-svc: h3=":443"; ma=86400
+server-timing: cfL4;desc="?proto=TCP&rtt=30068&min_rtt=30068&rtt_var=15034&sent=1&recv=3&lost=0&retrans=0&sent_bytes=0&recv_bytes=47&delivery_rate=0&cwnd=249&unsent_bytes=0&cid=0000000000000000&ts=0&x=0"
 
-Hello world!
- Close socket & disconnect ...
- End
---------------------------------------------------------------------------
+GET
+
+
+Test Eth TCP client
+[Eth]HTTP: Request message:
+GET /method HTTP/1.1
+Host: www.ifconfig.io
+
+
+
+[Eth]HTTP: Received 780 chars from server
+[Eth]HTTP: Received 200 OK status ... [OK]
+[Eth]HTTP: Received message:
+HTTP/1.1 200 OK
+Date: Thu, 26 Dec 2024 07:44:31 GMT
+Content-Type: text/plain; charset=utf-8
+Content-Length: 4
+Connection: keep-alive
+cf-cache-status: DYNAMIC
+Report-To: {"endpoints":[{"url":"https:\/\/a.nel.cloudflare.com\/report\/v4?s=VzRcGDcURxWffp2phmVYu51QWhkr%2FGtqk2KDZeG0AVWPgJ3f88Fwjyai%2Fw8GTSNDaG6gJKlv357FTqelXDVxsJxu5snuxATradH69HAE8V5Hg2nJn30Imsg8qZlF1GglH%2Bw%3D"}],"group":"cf-nel","max_age":604800}
+NEL: {"success_fraction":0,"report_to":"cf-nel","max_age":604800}
+Server: cloudflare
+CF-RAY: 8f7f7234df374a1c-TPE
+alt-svc: h3=":443"; ma=86400
+server-timing: cfL4;desc="?proto=TCP&rtt=29940&min_rtt=29940&rtt_var=14970&sent=1&recv=3&lost=0&retrans=0&sent_bytes=0&recv_bytes=47&delivery_rate=0&cwnd=249&unsent_bytes=0&cid=0000000000000000&ts=0&x=0"
+
+GET
+```
--- main.cpp
+++ main.cpp
@@ -4,155 +4,343 @@
 #include "TCPSocket.h"
 #include "EthernetInterface.h"
 
-#define MBED_HEAP_STATS_ENABLED 1
-#define USE_HTTP_1_1
+#define MAX_TEST_ROUNDS     3
 
+// Test connection information
+#define USE_HTTP_1_1
+#define HTTP_SERVER_NAME            "www.ifconfig.io"
+#define HTTP_SERVER_FILE_PATH       "/method"
+#define HTTP_SERVER_PORT            80
+#define HTTP_RECVBUFF_MAX           1024
 
 namespace {
-    // Test connection information
 
-const char *HTTP_SERVER_NAME = "www.ifconfig.io"; 
+char http_recv_buff[HTTP_RECVBUFF_MAX];
 
-const char *HTTP_SERVER_FILE_PATH = "/method";
-const int HTTP_SERVER_PORT = 80;
-
-
-    const int RECV_BUFFER_SIZE = 512;
-
-    // Test related data
-    const char *HTTP_OK_STR = "200 OK";
-    const char *HTTP_EXPECT_STR = "GET";
-
-    // Test buffers
-    char buffer[RECV_BUFFER_SIZE] = {0};
 }
 
-bool find_substring(const char *first, const char *last, const char *s_first, const char *s_last) {
-    const char *f = std::search(first, last, s_first, s_last);
-    return (f != last);
+// Case insensitive of std::strcmp
+int strcmp_ci(const char *s1, const char *s2)
+{
+    while (*s2) {
+        int d = toupper(*s1) - toupper(*s2);
+        if (d) {
+            return d;
+        }
+        s1 ++;
+        s2 ++;
+    }
+
+    return toupper(*s1);
 }
 
-void http_test(char *str, TCPSocket *sock) {
-    bool result = true;
+// Case insensitive of std::search
+const char *search_ci(const char *first, const char *last,
+                      const char *s_first, const char *s_last)
+{
+    auto bin_pred_ci = [](int c1, int c2) -> bool {
+        return toupper(c1) == toupper(c2);
+    };
+    return std::search(first, last, s_first, s_last, bin_pred_ci);
+}
+
+// Locate 'value' part of 'name:value'
+// NOTE: The returned 'value' part can have leadinging and/or trailing
+//       whitespace which needs further processing.
+const char *http_fieldvalue(const char *first, const char *last,
+                            const char *fieldname)
+{
+    size_t fieldname_len = strlen(fieldname);
+    const char *found = search_ci(first, last,
+                                  fieldname, fieldname + fieldname_len);
+    return (found == last) ? NULL : (found + fieldname_len + 1);
+}
+
+// Do one HTTP request/response round
+// NOTE: This will switch socket to non-blocking mode.
+void http_request_response(
+    const char *iface,
+    TCPSocket *sock,
+    const char *http_req,
+    char *http_resp_buff,
+    size_t http_resp_buffsize)
+{
+    /* Non-blocking mode
+     *
+     * Don't change to non-blocking mode before connect; otherwise, we may meet NSAPI_ERROR_IN_PROGRESS.
+     */
+    sock->set_blocking(false);
+      
     int rc = 0;
+    bool got200 = false;
+    size_t offset = 0;
+    size_t offset_end = strlen(http_req);
     
-        // We are constructing GET command like this:
+    printf("[%s]HTTP: Request message:\r\n", iface);
+    printf("%s", http_req);
+    printf("\r\n\r\n");
 
-       // GET /method HTTP/1.1\r\nHost: ifconfig.io\r\nConnection: close\r\n\r\n"
-        strcpy(buffer, "GET ");
-        strcat(buffer, HTTP_SERVER_FILE_PATH);   
-        strcat(buffer, " HTTP/1.1\r\nHost: ");
-        strcat(buffer, HTTP_SERVER_NAME);
-    #if 1
-        strcat(buffer, "\r\n\r\n");
-    #else
-        strcat(buffer, "\r\nConnection: close\r\n\r\n");
-    #endif
-        
-        // Send GET command
-        sock->send(buffer, strlen(buffer));
+    do {
+        rc = sock->send((const unsigned char *) http_req + offset, offset_end - offset);
+        if (rc > 0) {
+            offset += rc;
+        }
+    } while (offset < offset_end && 
+            (rc > 0 || rc == NSAPI_ERROR_WOULD_BLOCK));
+    if (rc < 0 &&
+        rc != NSAPI_ERROR_WOULD_BLOCK) {
+        printf("[%s]HTTP: socket send failed: %d", iface, rc);
+        return;
+    }
 
-        // Server will respond with HTTP GET's success code
-        const int ret = sock->recv(buffer, sizeof(buffer) - 1);
-        buffer[ret] = '\0';
-        
-        // Find 200 OK HTTP status in reply
-        bool found_200_ok = find_substring(buffer, buffer + ret, HTTP_OK_STR, HTTP_OK_STR + strlen(HTTP_OK_STR));
-        // Find "deny" string in reply
-        bool found_expect = find_substring(buffer, buffer + ret, HTTP_EXPECT_STR, HTTP_EXPECT_STR + strlen(HTTP_EXPECT_STR));
+    /* Read data out of the socket */
+    offset = 0;
+    offset_end = 0;
+    size_t content_length = 0;
+    char *line_beg = http_resp_buff;
+    char *line_end = NULL;
+    do {
+        rc = sock->recv((unsigned char *) http_resp_buff + offset, http_resp_buffsize - offset - 1);
+        if (rc > 0) {
+            offset += rc;
+        }
 
-        if (!found_200_ok) result = false;
-        if (!found_expect) result = false;
+        /* Make it null-terminated */
+        http_resp_buff[offset] = 0;
 
-        printf("[%s]HTTP: Received %d chars from server\r\n",str,ret);
-        printf("[%s]HTTP: Received 200 OK status ... %s\r\n",str, found_200_ok ? "[OK]" : "[FAIL]");
-        printf("[%s]HTTP: Received '%s' status ... %s\r\n",str, HTTP_EXPECT_STR, found_expect ? "[OK]" : "[FAIL]");
-        printf("[%s]HTTP: Received massage:\r\n\r\n", str);
-        printf("%s", buffer);
-    
+        /* Scan response message
+         *             
+         * 1. A status line which includes the status code and reason message (e.g., HTTP/1.1 200 OK)
+         * 2. Response header fields (e.g., Content-Type: text/html)
+         * 3. An empty line (\r\n)
+         * 4. An optional message body
+         */
+        if (! offset_end) {
+            // Scan received lines as possible
+            do {
+                line_end = strstr(line_beg, "\r\n");
+                if (line_end) {
+                    /* Scan status line */
+                    if (! got200) {
+                        got200 = strstr(line_beg, "200 OK") != NULL;
+                    }
+
+                    /* Scan response header fields for Content-Length 
+                     *
+                     * NOTE: Assume chunked transfer (Transfer-Encoding: chunked) is not used
+                     * NOTE: Support filed name case-insensitive
+                     * NOTE: Support both 'name:field' and 'name: field ' (any number of
+                     *       leading and/or trailing whitespace)
+                     */
+                    if (content_length == 0) {
+                        const char *fieldvalue = NULL;
+
+                        // The 'value' part can have leadinging and/or trailing whitespace.
+                        // Let scanf or the like handle it.
+                        fieldvalue = http_fieldvalue(line_beg, line_end, "content-length");
+                        if (fieldvalue != NULL) {
+                            sscanf(fieldvalue, "%d", &content_length);
+                        }
+                    }
+
+                    /* An empty line indicates end of response header fields */
+                    if (line_beg == line_end) {
+                        offset_end = line_end - http_resp_buff + 2 + content_length;
+                    }
+
+                    /* Go to next line */
+                    line_beg = line_end + 2;
+                }
+            } while (line_end != NULL);
+        }
+    } while ((offset_end == 0 || offset < offset_end) &&
+            (rc > 0 || rc == NSAPI_ERROR_WOULD_BLOCK));
+    if (rc < 0 && 
+        rc != NSAPI_ERROR_WOULD_BLOCK) {
+        printf("[%s]HTTP: socket recv failed: %d", iface, rc);
+        return;
+    }
+
+    http_resp_buff[offset] = '\0';
+
+    /* Print status messages */
+    printf("[%s]HTTP: Received %d chars from server\n", iface, offset);
+    printf("[%s]HTTP: Received 200 OK status ... %s\n", iface, got200 ? "[OK]" : "[FAIL]");
+    printf("[%s]HTTP: Received message:\n", iface);
+    printf("%s", http_resp_buff);
+    printf("\r\n\r\n");
 }
 
+void http_test(const char *iface, TCPSocket *sock)
+{
+    // We are constructing GET command like this:
+    // GET /method HTTP/1.1\r\nHost: ifconfig.io\r\nConnection: close\r\n\r\n"
+    const char *http_req =
+        "GET " HTTP_SERVER_FILE_PATH " HTTP/1.1\r\n"
+        "Host: " HTTP_SERVER_NAME "\r\n"
+        #if 0
+        "Connection: close\r\n"
+        #endif
+        "\r\n";
+    http_request_response(iface, sock, http_req, http_recv_buff, sizeof(http_recv_buff));
+}
 
-int main() {
+int main()
+{
 #if MBED_HEAP_STATS_ENABLED
     mbed_stats_heap_t heap_stats;
 #endif
 
-    printf("Start WiFi test \r\n");
-     
-    bool result = true;
     int rc = 0;
+    bool has_eth = true;
+    SocketAddress sockaddr;
 
     printf("Start Connection ... \r\n");
 
     // WiFi part
+    TCPSocket wifi_sock;
     NetworkInterface *wifi_network_interface = WiFiInterface::get_default_instance();
     if (NULL == wifi_network_interface) {
         printf("NULL network interface! Exiting application....\r\n");
-        return 0;
+        return EXIT_FAILURE;
     }
     wifi_network_interface->set_default_parameters();
-    printf("\n\rUsing WiFi \r\n");
+
     printf("\n\rConnecting to WiFi..\r\n");
     rc = wifi_network_interface->connect();
     if(rc == 0) {
-        printf("\n\rConnected to Network successfully\r\n");
+        printf("\n\rConnected to Network (WiFi) successfully\r\n");
     } else {
-        printf("\n\rConnection to Network Failed %d! Exiting application....\r\n", rc);
-        return 0;
-    }    
-    printf("WiFi TCP client IP Address is %s\r\n", wifi_network_interface->get_ip_address());
-    TCPSocket wifi_sock(wifi_network_interface);
+        printf("\n\rConnection to Network (WiFi) Failed %d! Exiting application....\r\n", rc);
+        return EXIT_FAILURE;
+    }
+    rc = wifi_network_interface->get_ip_address(&sockaddr);
+    if (rc != NSAPI_ERROR_OK) {
+        printf("Network interface (WiFi) get_ip_address(...) failed with %d\r\n", rc);
+        return EXIT_FAILURE;
+    } else {
+        printf("WiFi TCP client IP Address is %s\r\n", sockaddr.get_ip_address());
+    }
+
+    rc = wifi_sock.open(wifi_network_interface);
+    if (rc != NSAPI_ERROR_OK) {
+        printf("TCP socket open (WiFi) failed with %d", rc);
+        return EXIT_FAILURE;
+    }
 
     //Eth part
+    TCPSocket eth_sock;
     NetworkInterface *eth_network_interface = EthernetInterface::get_default_instance();
-    
-    eth_network_interface->connect(); //Use DHCP
-        
-    printf("Eth TCP client IP Address is %s\r\n", eth_network_interface->get_ip_address());
-
-    TCPSocket eth_sock(eth_network_interface);
-
-
-    printf("ETH HTTP Connection ... \r\n");
-    if (eth_sock.connect(HTTP_SERVER_NAME, HTTP_SERVER_PORT) == 0) {
-        printf("ETH HTTP: Connected to %s:%d\r\n", HTTP_SERVER_NAME, HTTP_SERVER_PORT);
+    if (eth_network_interface == NULL) {
+        printf("\n\rNo ETH\r\n");
+        has_eth = false;
+        goto DO_HTTP_CONN;
     }
+
+    printf("\n\rConnecting to ETH..\r\n");
+    rc = eth_network_interface->connect(); //Use DHCP
+    if(rc == 0) {
+        printf("\n\rConnected to Network (ETH) successfully\r\n");
+    } else {
+        printf("\n\rConnection to Network (ETH) Failed %d!\r\n", rc);
+        has_eth = false;
+        goto DO_HTTP_CONN;
+    }
+
+    rc = eth_network_interface->get_ip_address(&sockaddr);
+    if (rc != NSAPI_ERROR_OK) {
+        printf("Network interface (ETH) get_ip_address(...) failed with %d\r\n", rc);
+        has_eth = false;
+        goto DO_HTTP_CONN;
+    } else {
+        printf("Eth TCP client IP Address is %s\r\n", sockaddr.get_ip_address());
+    }
+
+    rc = eth_sock.open(eth_network_interface);
+    if (rc != NSAPI_ERROR_OK) {
+        printf("TCP socket open (ETH) failed with %d", rc);
+        has_eth = false;
+        goto DO_HTTP_CONN;
+    }
+
+DO_HTTP_CONN:
+
+    printf("\r\n");
+
     printf("WiFi HTTP Connection ... \r\n");
-    if (wifi_sock.connect(HTTP_SERVER_NAME, HTTP_SERVER_PORT) == 0) {
-        printf("WiFi HTTP: Connected to %s:%d\r\n", HTTP_SERVER_NAME, HTTP_SERVER_PORT);
-    }
-
-    while(1) {
-        printf("Test Eth TCP client\r\n");    
-        http_test("Eth",&eth_sock);
-        wait(2);
-        printf("Test WiFi TCP client\r\n");
-        http_test("WiFi",&wifi_sock);
-        wait(2);
-    }
-#if MBED_HEAP_STATS_ENABLED
-    mbed_stats_heap_get(&heap_stats);
-    printf("Current heap: %lu\r\n", heap_stats.current_size);
-    printf("Max heap size: %lu\r\n", heap_stats.max_size);
-#endif
-    while(1) {
-        int in_char = getchar();
-        if (in_char == 'h') {
-            mbed_stats_heap_get(&heap_stats);
-            printf("Current heap: %lu\r\n", heap_stats.current_size);
-            printf("Max heap size: %lu\r\n", heap_stats.max_size);
-            continue;
-        } else if (in_char == 'q') {
-            break;
+    rc = wifi_network_interface->gethostbyname(HTTP_SERVER_NAME, &sockaddr);
+    if (rc != NSAPI_ERROR_OK) {
+        printf("WiFi HTTP Connection ... DNS failure\r\n");
+    } else {
+        sockaddr.set_port(HTTP_SERVER_PORT);
+        rc = wifi_sock.connect(sockaddr);
+        if (rc != NSAPI_ERROR_OK) {
+            printf("WiFi HTTP Connection ... Failed\r\n");
+        } else {
+            printf("WiFi HTTP: Connected to %s:%d\r\n", HTTP_SERVER_NAME, HTTP_SERVER_PORT);
         }
     }
 
+    if (!has_eth) {
+        goto DO_HTTP_SEND_RECV;
+    }
+
+    printf("ETH HTTP Connection ... \r\n");
+    rc = eth_network_interface->gethostbyname(HTTP_SERVER_NAME, &sockaddr);
+    if (rc != NSAPI_ERROR_OK) {
+        printf("ETH HTTP Connection ... DNS failure\r\n");
+    } else {
+        sockaddr.set_port(HTTP_SERVER_PORT);
+        rc = eth_sock.connect(sockaddr);
+        if (rc != NSAPI_ERROR_OK) {
+            printf("ETH HTTP Connection ... Failed\r\n");
+        } else {
+            printf("ETH HTTP: Connected to %s:%d\r\n", HTTP_SERVER_NAME, HTTP_SERVER_PORT);
+        }
+    }
+
+DO_HTTP_SEND_RECV:
+
+    printf("\r\n");
+
+    int test_round = 0;
+    while(1) {
+        printf("\r\n");
+
+#if MBED_HEAP_STATS_ENABLED
+        mbed_stats_heap_get(&heap_stats);
+        printf("Current heap: %lu\r\n", heap_stats.current_size);
+        printf("Max heap size: %lu\r\n", heap_stats.max_size);
+        printf("\r\n");
+#endif
+
+        test_round ++;
+        if (test_round > MAX_TEST_ROUNDS) {
+            break;
+        }
+        printf("Test round %d ... \r\n\r\n", test_round);
+
+        printf("Test WiFi TCP client\r\n");
+        http_test("WiFi",&wifi_sock);
+        ThisThread::sleep_for(2s);
+
+        if (eth_network_interface == NULL) {
+            continue;
+        }
+
+        printf("Test Eth TCP client\r\n");
+        http_test("Eth",&eth_sock);
+        ThisThread::sleep_for(2s);
+    }
+
     printf(" Close socket & disconnect ... \r\n");
-    eth_sock.close();
     wifi_sock.close();
-    
     wifi_network_interface->disconnect();
-    eth_network_interface->disconnect();
+    // Redundant socket close won't do harm
+    eth_sock.close();
+    // Redundant network interface disconnect won't do harm
+    if (eth_network_interface != NULL) {
+        eth_network_interface->disconnect();
+    }
     printf(" End \r\n");
 }
